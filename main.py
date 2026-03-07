be2bbc82c2fdb4bd582ec1e7bf0def6cc1186988
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-03-07 15:30:25 -0300

    REFACTOR: Updating the Welcome Message in ./main.py

--- main.py
+++ main.py
@@ -1641,7 +1641,7 @@ def main():
     """
 
     print(
-        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Main Template Python{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
+        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Latex Reviewer Python Project{BackgroundColors.GREEN}!{Style.RESET_ALL}",
         end="\n\n",
     )  # Output the welcome message
     start_time = datetime.datetime.now()  # Get the start time of the program
